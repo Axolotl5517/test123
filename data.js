caaba54d4a4d70cd3659f459e65ca8245640f6b5
Axolotl5517 <funnl09517@gmail.com>
2025-06-24 23:15:22 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "弘仁醫院",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.09865443460245338,
+        "pitch": -0.2741998364976279,
+        "fov": 1.299695808558111
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.30110168790590564,
+          "pitch": -0.300098188790356,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">宏仁醫院</font></font>",
+          "text": "<div style=\"text-align: center;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">日治時期由前嘉義市長方輝龍醫師所創建，後來由其子方士漆龍醫師接手經營收益，是一家近九十年歷史的老醫館。</font></font></div>"
+        }
+      ]
+    }
+  ],
+  "name": "弘仁醫院",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
